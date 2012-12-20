b4b5f8acc405df5ed3482ac353d3e7c8ce6648e9
Yexi Jiang <yexijiang@gmail.com>
2012-12-20 15:57:04 -0500

    local change

---
+++ SystemMonitoring/src/sysmon/manager/ManagerCLI.java
@@ -0,0 +1,10 @@
+package sysmon.manager;
+
+/**
+ * A CLI to interact with 
+ * @author yexijiang
+ *
+ */
+public class ManagerCLI {
+
+}
---
+++ SystemMonitoring/src/sysmon/manager/MonitorAssigner.java
@@ -0,0 +1,27 @@
+package sysmon.manager;
+
+import java.util.Map;
+
+import sysmon.manager.MonitoringManager.CollectorProfile;
+
+/**
+ * Assign the monitor to proper collector.
+ * @author yexijiang
+ *
+ */
+public abstract class MonitorAssigner {
+	
+	protected Map<String, CollectorProfile> collectorsProfiles;
+	
+	public MonitorAssigner(Map<String, CollectorProfile> collectorsProfiles) {
+		this.collectorsProfiles = collectorsProfiles;
+	}
+	
+	/**
+	 * Assign the given monitor to proper collector.
+	 * @param monitorName
+	 * @return
+	 */
+	public abstract String assignMonitorToCollector(String monitorName);
+
+}
